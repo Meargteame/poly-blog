8ed67c68054308320dc974d7d95d0b17a512d36e
Meargteame <mearegteame99995555@gmail.com>
2025-01-04 01:33:59 +0300

    register function created to send input from user to backend

--- client/polyblog/src/pages/LoginPage.jsx
+++ client/polyblog/src/pages/LoginPage.jsx
@@ -1,12 +1,23 @@
-export default function LoginPage() {
-    return (
-        <form className="login">
-            <h1>Login  </h1>
-            <input type="text" placeholder="username"/>
-            <input type="password" placeholder="password"/>
-            <button> Login </button>
-        </form>
-    )
+import { useState } from "react"
+
+export default function RegisterPage(){
+  
+        const [username,setUsername] = useState('');
+        const [password,setPassword] = useState('');  
+
+        function register(ev){
+            ev.preventDefault()
+            fetch('https://localhost:4000',{
+                method:'POST',
+                body:JSON.stringify(username,password)
+            })
+        }
+      return (
+          <form className="register" onSubmit={register}>
+              <h1>Login</h1>
+              <input type="text" value={username} placeholder="username"  onChange={ e => { setUsername(e.target.value)}}/>
+              <input type="password" value={password} placeholder="password" onChange={ e => { setPassword(e.target.value)}}/> 
+              <button> Login  </button>
+          </form>
+      )
   }
-
-
--- client/polyblog/src/pages/RegisterPage.jsx
+++ client/polyblog/src/pages/RegisterPage.jsx
@@ -1,26 +1,16 @@
+import { useState } from "react"
 export default function RegisterPage(){
-    const [username,setUsername] = useState("");
-    const [password,setPassword] = useState("");
+
+    const [username,setUsername] = useState('');
+    const [password,setPassword] = useState('');
 
 
     return (
-        <form  className="register" >
-            <h1>Register  </h1>
-            <input type="text" 
-            placeholder="username"
-            value={username}
-            onChange={e=>{
-                setUsername(e.target.value)
-            }}
-            />
-            <input  type="password"
-             placeholder="password"
-             value={password}
-             onChange={e=>{
-                setPassword(e.target.value)
-             }}
-             />
-            <button>Register</button>
+        <form className="register">
+            <h1>Register</h1>
+            <input type="text" placeholder="username" value={username} onChange={ e => {setUsername(e.target.value)}} />
+            <input type="password" placeholder="password" value={password} onChange={ e => {setPassword(e.target.value)}} />
+            <button> Register Now </button>
         </form>
     )
 }
