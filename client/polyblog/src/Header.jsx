9436fae2f7d701c06b38686a6d128851a165b1bb
Meargteame <mearegteame99995555@gmail.com>
2025-01-03 23:31:29 +0300

    authentication is updated

--- client/polyblog/src/Header.jsx
+++ client/polyblog/src/Header.jsx
@@ -3,7 +3,8 @@ import { Link } from "react-router-dom";
 export default function Header(){
     return (
         <header>
-            <a to="/" className='logo'>PolyBlog </a>
+            {/* <a to="/" className='logo'>PolyBlog </a> */}
+            <Link to='/' className="logo">PolyBlog</Link>
             <nav>
             <Link to='/login' > Login </Link>
             <Link to='/register' > Register  </Link>
--- client/polyblog/src/pages/RegisterPage.jsx
+++ client/polyblog/src/pages/RegisterPage.jsx
@@ -1,9 +1,25 @@
 export default function RegisterPage(){
+    const [username,setUsername] = useState("");
+    const [password,setPassword] = useState("");
+
+
     return (
         <form  className="register" >
-            <h1>Register</h1>
-            <input type="text" placeholder="username"/>
-            <input  type="password" placeholder="password"/>
+            <h1>Register  </h1>
+            <input type="text" 
+            placeholder="username"
+            value={username}
+            onChange={e=>{
+                setUsername(e.target.value)
+            }}
+            />
+            <input  type="password"
+             placeholder="password"
+             value={password}
+             onChange={e=>{
+                setPassword(e.target.value)
+             }}
+             />
             <button>Register</button>
         </form>
     )
