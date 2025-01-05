2afbc5820148ddd30cbede67c738285580eb089f
Meargteame <mearegteame99995555@gmail.com>
2025-01-05 11:02:46 +0300

    User model imported to main backend

--- api/index.js
+++ api/index.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const cors = require('cors');
-
+const { default: mongoose } = require('mongoose');
+const User = require('./models/User');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+
+
+
+mongoose.connect('mongodb+srv://poly:05uXPFqeySH44SSu@cluster0.i3mni.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 // Register API
 app.post('/register', (req, res) => {
-  const { username, password } = req.body; // Correct destructuring
+  const { username, password } = req.body; 
+  User.create({
+    
+  })
   res.json({ requestedData: { username, password } });
 });
 
 // Login API
 app.post('/login', (req, res) => {
-  const { username, password } = req.body; // Correct destructuring
+  const { username, password } = req.body; 
   console.log(username, password);
   res.json({ requestedData: { username, password } });
 });
@@ -28,5 +36,5 @@ app.listen(4000, () => {
 
 
 // {
-//     mongodb+srv://poly:05uXPFqeySH44SSu@cluster0.i3mni.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+//     
 // }
