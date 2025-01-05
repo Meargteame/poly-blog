dd2cf82a5f2b2ab090d6e828e85f447a8ed690e6
Meargteame <mearegteame99995555@gmail.com>
2025-01-05 10:36:06 +0300

    the register function now catchs errors

---
+++ client/polyblog/src/pages/RegisterPage.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+export default function RegisterPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function register(ev) {
+    ev.preventDefault();
+    try {
+      const response = await fetch("http://localhost:4000/register", {
+        method: 'POST',
+        body: JSON.stringify({ username, password }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      const data = await response.json();
+      console.log('Response from backend:', data);
+    } catch (error) {
+      console.error('Error during registration:', error);
+    }
+  }
+
+  return (
+    <form className="register" onSubmit={register}>
+      <h1>Register</h1>
+      <input
+        type="text"
+        placeholder="username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Register Now</button>
+    </form>
+  );
+}
