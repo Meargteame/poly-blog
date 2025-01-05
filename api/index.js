fee24f24220e6f82980ea2f0cd6d5aa036c6153e
Meargteame <mearegteame99995555@gmail.com>
2025-01-05 11:04:43 +0300

    connection established with mongodb database

--- api/index.js
+++ api/index.js
@@ -8,9 +8,9 @@ app.use(cors());
 app.use(express.json());
 
 
-
-
 mongoose.connect('mongodb+srv://poly:05uXPFqeySH44SSu@cluster0.i3mni.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+
+
 // Register API
 app.post('/register', (req, res) => {
   const { username, password } = req.body; 
