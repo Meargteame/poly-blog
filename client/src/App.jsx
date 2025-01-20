9f3b5d23d66877c601fb8d0f0cacb01785162556
meargteame <mearegteame99995555@gmail.com>
2025-01-20 11:15:45 -0800

    the login and register links are done

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+      <main>
+        <header> 
+          <a className='logo'>
+            Poly-Blog 
+          </a>
+          <nav>
+            <a href=''>Login</a>
+            <a href=''>Register</a>
+          </nav>
+        </header>
+      </main>
+  )
+}
+
+export default App
