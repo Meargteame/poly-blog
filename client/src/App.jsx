2bae5319a66ee48c8c1aeead9de9dc55fff36d22
meargteame <mearegteame99995555@gmail.com>
2025-01-20 11:41:19 -0800

    A div entry created with main things in it for the spefic blog post

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,6 +16,24 @@ function App() {
             <a href=''>Register</a>
           </nav>
         </header>
+        <div className='entry'>
+          <img src="https://images.app.goo.gl/FHxVL3nLk1cyV26N8" alt='Random blog image from the internet '/>
+          <h2> Full house battery backup comig later this year </h2>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati amet dolores atque vitae adipisci consequuntur ut repudiandae odit sequi porro, hic impedit. Vel officiis veritatis dolore quaerat nesciunt repellendus ex!
+          Quaerat adipisci hic repudiandae debitis recusandae deserunt mollitia quibusdam ab ipsam repellat, enim laborum voluptates omnis pariatur. Quaerat soluta hic nemo maxime in, cum dignissimos. Illum voluptatem rem non nisi.</p>
+        </div>
+        <div className='entry'>
+          <img src="https://images.app.goo.gl/FHxVL3nLk1cyV26N8" alt='Random blog image from the internet '/>
+          <h2> Full house battery backup comig later this year </h2>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati amet dolores atque vitae adipisci consequuntur ut repudiandae odit sequi porro, hic impedit. Vel officiis veritatis dolore quaerat nesciunt repellendus ex!
+          Quaerat adipisci hic repudiandae debitis recusandae deserunt mollitia quibusdam ab ipsam repellat, enim laborum voluptates omnis pariatur. Quaerat soluta hic nemo maxime in, cum dignissimos. Illum voluptatem rem non nisi.</p>
+        </div>
+        <div className='entry'>
+          <img src="https://images.app.goo.gl/FHxVL3nLk1cyV26N8" alt='Random blog image from the internet '/>
+          <h2> Full house battery backup comig later this year </h2>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati amet dolores atque vitae adipisci consequuntur ut repudiandae odit sequi porro, hic impedit. Vel officiis veritatis dolore quaerat nesciunt repellendus ex!
+          Quaerat adipisci hic repudiandae debitis recusandae deserunt mollitia quibusdam ab ipsam repellat, enim laborum voluptates omnis pariatur. Quaerat soluta hic nemo maxime in, cum dignissimos. Illum voluptatem rem non nisi.</p>
+        </div>
       </main>
   )
 }
