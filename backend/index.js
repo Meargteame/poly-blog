0fee26c68033105c0e8086e4a46220a442ab3a12
meargteame <mearegteame99995555@gmail.com>
2025-01-23 11:58:38 -0800

    login and register funcionality logic implemented

--- backend/index.js
+++ backend/index.js
@@ -3,14 +3,20 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const User = require('./models/User'); 
 const bcrypt = require('bcryptjs');
-
+const jwt = require('jsonwebtoken');
 const app = express();
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
 const PORT = 4000;
 
 const salt = bcrypt.genSaltSync(10);
-
+const secret = 'hdsjfhuy432jhjhfsfsdfdsfds'
 // Middleware to enable CORS
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173', // Your frontend's origin
+  credentials: true, // Allow credentials (cookies, etc.)
+}));
+// 
 app.use(express.json())
 
 mongoose.connect('mongodb://localhost:27017/crud')
@@ -46,13 +52,24 @@ app.post('/register', async (req, res) => {
 // loggin endpoint 
 
 
-app.post('/login', async (req, res) => {
-  const { username, password } = req.body;
-  const userDoc = await User.findOne({ username });
-  res.json(userDoc);
-});
+app.post('/login',async (req,res)=>{
+  const { username,password} = req.body;
+  const userDoc = await User.findOne({username});
+  const passOk = bcrypt.compareSync(password,userDoc.password);
 
+  if(passOk){
+     jwt.sign({username,id:userDoc.id},secret,{},(err,token)=>{
+      if(err) throw err;
+      res.cookie('token',token).json('OK!');
+    })
+  }else{
+    res.status(400).json('wrong credentials!')
+  }
+})
 
+app.get('/profile',(req,res) =>{
+  res.json(req.cookies);
+})
 
 app.listen(4000, () => {
   console.log(`Server running on port ${PORT}`);
